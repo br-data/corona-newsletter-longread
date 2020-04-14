063a11427f9792253881e8ad8a99516bd6cf575b
Steffen Kühne <stekhn@gmail.com>
2020-04-14 21:42:19 +0200

    Add simple prototype

---
+++ src/scripts/custom/text/bayern.js
@@ -0,0 +1,51 @@
+export function init(config) {
+  const { selector, data } = config;
+
+  const text = `Bislang wurden ${pretty(currentCount(data))} Corona-Fälle in Bayern  gemeldet. Das sind ${pretty(currentIncrease(data))} Fälle (${pretty(currentIncreasePerc(data))} %) mehr als noch am Vortag. Die Zahl der gemeldeten Fälle in Bayern verdoppelt sich damit ungefähr alle ${doublingTime(data)} Tage.
+
+  Alle Angaben und Berechnungen beziehen sich auf die aktuellen Zahlen des Robert Koch-Instituts vom ${germanDate(currentDate(data))}.`;
+
+  const textElement = document.querySelector(selector);
+  textElement.textContent = text;
+}
+
+function currentCount(data) {
+  return data[data.length-1].sumValue;
+}
+
+function currentIncrease(data) {
+  return data[data.length-1].value;
+}
+
+function currentIncreasePerc(data) {
+  const currentDay = data[data.length-1].sumValue;
+  const previousDay = data[data.length-2].sumValue;
+  return (currentDay - previousDay) / currentDay * 100;
+}
+
+function currentDate(data) {
+  return data[data.length-1].Meldedatum;
+}
+
+function doublingTime(data) {
+  const date1 = new Date(data[data.length-2].Meldedatum);
+  const value1 = data[data.length-2].sumValue;
+  const date2 = new Date(data[data.length-9].Meldedatum);
+  const value2 = data[data.length-9].sumValue;
+  const diff = Math.ceil(date2.getTime() - date1.getTime()) / 86400000;
+  const days = (Math.log(2) * diff) / (Math.log(value2) - Math.log(value1));
+  const minDays = Math.floor(days);
+  const maxDays = Math.ceil(days);
+  return `${minDays} bis ${maxDays}`;
+}
+
+function germanDate(dateString) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = new Date(dateString);
+  return date.toLocaleDateString('de-DE', options);
+}
+
+function pretty(number) {
+  const string = (Math.round(number * 10) / 10).toString().split('.');
+  return string[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.') + (string[1] ? `,${string[1]}` : '');
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,16 +5,42 @@ import 'intersection-observer';
 import * as lazyload from './components/lazyload';
 import * as marginals from './components/marginals';
 import * as navigation from './components/navigation';
-import * as analytics from './components/analytics/analytics';
 
-import Chart from './custom/chart/chart';
-import chartData from './custom/chart/chart-data.json';
+import * as bayernText from './custom/text/bayern';
+import * as bayernLandkreiseText from './custom/text/bayern-landkreise';
 
-let chart;
+// Mock data for testing
+import bayernData from './data/bayern.json';
+import bayernLandkreiseData from './data/bayern-landkreise.json';
+
+// import Chart from './custom/chart/chart';
+// import chartData from './custom/chart/chart-data.json';
+// let chart;
 
 window.addEventListener('load', init);
 
-function init() {
+async function init() {
+
+  // const bayernData = await fetch('https://europe-west3-brdata-corona.cloudfunctions.net/rkiApi/query?startDate=2020-03-12&group=Bundesland&bundesland=Bayern').then((response) => { return response.json(); });
+
+  bayernText.init({
+    selector: '#bayern',
+    data: bayernData
+  });
+
+  // const bayernLandkreiseData = await fetch('https://europe-west3-brdata-corona.cloudfunctions.net/rkiApi/query?group=Landkreis&bundesland=Bayern').then((response) => { return response.json(); });
+
+  bayernLandkreiseText.init({
+    selector: '#bayern-landkreise',
+    data: bayernLandkreiseData
+  });
+
+  // const deutschlandData = await fetch('https://europe-west3-brdata-corona.cloudfunctions.net/rkiApi/query?startDate=2020-03-12');
+
+  // const regBezData = await fetch('https://europe-west3-brdata-corona.cloudfunctions.net/rkiApi/query?startDate=2020-03-12&group=Regierungsbezirk&bundesland=Bayern');
+
+  // const landkreisData = await fetch('https://europe-west3-brdata-corona.cloudfunctions.net/lglApi/date');
+
   lazyload
     .init('.lazyload', {
       rootMargin: '300px 0px 300px 0px',
@@ -25,36 +51,22 @@ function init() {
   navigation.init();
   marginals.init();
 
-  chart = new Chart({
-    target: '#chart',
-    data: chartData
-  });
+  // chart = new Chart({
+  //   target: '#chart',
+  //   data: chartData
+  // });
 
-  analytics.init({
-    serviceUrl: 'https://ddj.br.de/analytics/track',
-    projectId: 'test',
-    tracker: {
-      client: true,
-      click: true,
-      observer: true,
-      timer: true,
-      custom: true
-    },
-    respectDoNotTrack: false,
-    debug: false
-  });
-
-  resize();
+  // resize();
 }
 
-function resize() {
-  let timeout;
+// function resize() {
+//   let timeout;
 
-  window.onresize = () => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      marginals.update();
-      chart.update();
-    }, 200);
-  };
-}
+//   window.onresize = () => {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       marginals.update();
+//       chart.update();
+//     }, 200);
+//   };
+// }
