3e1fc4b1a38ad519aefd3bba47c416b48ad8f098
Steffen Kühne <stekhn@gmail.com>
2020-10-23 11:27:34 +0200

    Refactor bar chart to use SVG

---
+++ src/scripts/chart/bar-chart-canvas.js
@@ -0,0 +1,197 @@
+import { select, create } from 'd3-selection';
+import { max, min } from 'd3-array';
+import { scaleLinear, scaleBand } from 'd3-scale';
+import { line, curveMonotoneX } from 'd3-shape';
+import { sma } from '../utils';
+
+import { pretty, germanDate, germanDateShort, dateRange, getRetinaRatio } from '../utils';
+
+const defaults = {
+  target: '#bar-chart',
+  margin: { top: 130, right: 25, bottom: 75, left: 25 }
+};
+
+export default class BarChart {
+
+  constructor(config) {
+    this.set(config);
+    this.draw();
+  }
+
+  set(config) {
+    Object.assign(this, defaults, config);
+  }
+
+  draw() {
+    const { target, data, meta, margin } = this;
+    const container = select(target);
+    const ratio = getRetinaRatio();
+
+    // Set initial dimensions
+    const width = container.node().getBoundingClientRect().width;
+    const height = (width * 9) / 16;
+
+    // Create canvas and context
+    const canvas = create('canvas')
+      .attr('width', width * ratio)
+      .attr('height', height * ratio)
+      .style('width', `${width}px`)
+      .style('height', `${height}px`)
+      .node();
+
+    const context = canvas.getContext('2d');
+
+    // Scale canvas by retina ratio
+    context.scale(ratio, ratio);
+
+    // Draw background gradient
+    const gradient = context.createRadialGradient(
+      width / 2,
+      height / 2,
+      height / 4,
+      width / 2,
+      height / 2,
+      height
+    );
+
+    gradient.addColorStop(0, '#484B5A');
+    gradient.addColorStop(1, '#1D2029');
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, width, height);
+
+    // Set canvas default font
+    context.font = '300 14px "Open Sans", OpenSans, Arial';
+
+    // Adjust for margins
+    context.translate(margin.left, margin.top);
+
+    // Set drawing dimensions
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const xMax = max(data, d => new Date(d.date));
+    const xMin = min(data, d => new Date(d.date));
+    xMin.setDate(xMin.getDate() - 2);
+    const yMax = max(data, d => d.value);
+
+    const x = scaleBand()
+      .paddingOuter(0.1)
+      .paddingInner(0.4)
+      .align(0.5)
+      .domain(data.map(d => d.date))
+      .rangeRound([0, innerWidth]);
+
+    const y = scaleLinear()
+      .domain([0, yMax])
+      .range([innerHeight, 0]);
+
+    const xTicks = dateRange(xMin, xMax, Math.floor(data.length / 6));
+
+    // Draw x axis
+    context.textAlign = 'center';
+    context.textBaseline = 'top';
+    xTicks.forEach(d => {
+      context.fillStyle = '#ffffff';
+      context.fillText(germanDateShort(d), x(d) + x.bandwidth() / 2, innerHeight + 5);
+    });
+
+    // Draw y axis
+    context.beginPath();
+    context.lineWidth = 1;
+    y.ticks(3).forEach(d => {
+      context.moveTo(0, y(d));
+      context.lineTo(innerWidth, y(d));
+    });
+    context.strokeStyle = '#6d7182';
+    context.stroke();
+
+    context.textAlign = 'left';
+    context.textBaseline = 'bottom';
+    y.ticks(3).forEach(d => {
+      context.fillStyle = '#ffffff';
+      context.fillText(pretty(d), 0, y(d) - 2);
+    });
+
+    // Draw bars
+    context.fillStyle = '#0b9fd8';
+    data.forEach(d => {
+      context.fillRect(x(d.date), y(d.value), x.bandwidth(), innerHeight - y(d.value));
+    });
+
+    // Draw line
+    const lineConstructor = line()
+      .x(d => x(d.date) + (x.bandwidth() / 2))
+      .y(d => y(d.value))
+      .curve(curveMonotoneX)
+      .context(context);
+
+    context.setLineDash([10, 10]);
+    context.beginPath();
+    context.lineCap = 'round';
+    lineConstructor(sma(data, 7, 'value'));
+    context.lineWidth = 3;
+    context.strokeStyle = '#ffffff';
+    context.stroke();
+
+    // Add title
+    context.font = '600 24px "Open Sans", OpenSans, Arial';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+    context.fillStyle = '#ffffff';
+    context.fillText(meta.title, 0, -margin.top + 20);
+
+    // Add description
+    context.font = '300 15px "Open Sans", OpenSans, Arial';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+    context.fillStyle = '#9fa3b3';
+    context.fillText(meta.description, 0, -margin.top + 50);
+
+    // Add key
+    context.beginPath();
+    context.rect(0, -margin.top + 85, 11, 11);
+    context.fillStyle = '#0b9fd8';
+    context.fill();
+
+    context.font = '300 15px "Open Sans", OpenSans, Arial';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+    context.fillStyle = '#ffffff';
+    context.fillText('Neuinfektionen', 18, -margin.top + 85);
+
+    context.setLineDash([5, 5]);
+    context.beginPath();
+    context.moveTo(145, -margin.top + 91);
+    context.lineTo(175, -margin.top + 91);
+    context.lineCap = 'round';
+    context.lineWidth = 3;
+    context.stroke();
+
+    context.font = '300 15px "Open Sans", OpenSans, Arial';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+    context.fillStyle = '#ffffff';
+    context.fillText('7-Tage-Mittelwert', 180, -margin.top + 85);
+
+    // Add author and source
+    context.font = '300 14px "Open Sans", OpenSans, Arial';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+    context.fillStyle = '#9fa3b3';
+    context.fillText(`Grafik: ${meta.author}, Daten: ${meta.source} (Stand: ${germanDate(meta.date)})`, 0, innerHeight + 40);
+
+    // Scale canvas by pixel density
+    context.scale(1, 1);
+
+    // Create image from canvas and append it to the DOM
+    container.append('img')
+      .attr('src', canvas.toDataURL())
+      .attr('title', meta.title)
+      .attr('alt', `${meta.title}: ${meta.description}`);
+  }
+
+  update() {
+    select(this.target).html('');
+    this.draw();
+  }
+}
--- src/scripts/chart/bar-chart.js
+++ src/scripts/chart/bar-chart.js
@@ -1,10 +1,11 @@
-import { select, create } from 'd3-selection';
+import { select } from 'd3-selection';
 import { max, min } from 'd3-array';
 import { scaleLinear, scaleBand } from 'd3-scale';
 import { line, curveMonotoneX } from 'd3-shape';
+import { axisRight, axisBottom } from 'd3-axis';
 import { sma } from '../utils';
 
-import { pretty, germanDate, germanDateShort, dateRange, getRetinaRatio } from '../utils';
+import { pretty, germanDate, germanDateShort, dateRange } from '../utils';
 
 const defaults = {
   target: '#bar-chart',
@@ -25,169 +26,201 @@ export default class BarChart {
   draw() {
     const { target, data, meta, margin } = this;
     const container = select(target);
-    const ratio = getRetinaRatio();
 
     // Set initial dimensions
     const width = container.node().getBoundingClientRect().width;
     const height = (width * 9) / 16;
 
-    // Create canvas and context
-    const canvas = create('canvas')
-      .attr('width', width * ratio)
-      .attr('height', height * ratio)
-      .style('width', `${width}px`)
-      .style('height', `${height}px`)
-      .node();
-
-    const context = canvas.getContext('2d');
-
-    // Scale canvas by retina ratio
-    context.scale(ratio, ratio);
-
-    // Draw background gradient
-    const gradient = context.createRadialGradient(
-      width / 2,
-      height / 2,
-      height / 4,
-      width / 2,
-      height / 2,
-      height
-    );
-
-    gradient.addColorStop(0, '#484B5A');
-    gradient.addColorStop(1, '#1D2029');
-    context.fillStyle = gradient;
-    context.fillRect(0, 0, width, height);
-
-    // Set canvas default font
-    context.font = '300 14px "Open Sans", OpenSans, Arial';
-
-    // Adjust for margins
-    context.translate(margin.left, margin.top);
-
     // Set drawing dimensions
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
-    const xMax = max(data, d => new Date(d.date));
+    // Calculate horizontal scale and axis
     const xMin = min(data, d => new Date(d.date));
-    xMin.setDate(xMin.getDate() - 2);
-    const yMax = max(data, d => d.value);
+    const xMinBracket = new Date(xMin);
+    xMinBracket.setDate(xMinBracket.getDate() - 8);
 
-    const x = scaleBand()
-      .paddingOuter(0.1)
-      .paddingInner(0.4)
-      .align(0.5)
-      .domain(data.map(d => d.date))
-      .rangeRound([0, innerWidth]);
+    const xMax = max(data, d => new Date(d.date));
+    const xMaxBracket = new Date(xMax);
+    xMaxBracket.setDate(xMaxBracket.getDate() + 8);
 
-    const y = scaleLinear()
-      .domain([0, yMax])
-      .range([innerHeight, 0]);
-
+    const xValues = dateRange(xMinBracket, xMaxBracket, 1);
     const xTicks = dateRange(xMin, xMax, Math.floor(data.length / 6));
 
-    // Draw x axis
-    context.textAlign = 'center';
-    context.textBaseline = 'top';
-    xTicks.forEach(d => {
-      context.fillStyle = '#ffffff';
-      context.fillText(germanDateShort(d), x(d) + x.bandwidth() / 2, innerHeight + 5);
-    });
+    const x = scaleBand()
+      .paddingOuter(0)
+      .paddingInner(.4)
+      .domain(xValues)
+      .range([0, innerWidth]);
+    
+    // Calculate vertical scale and axis
+    const yMax = max(data, d => d.value);
 
-    // Draw y axis
-    context.beginPath();
-    context.lineWidth = 1;
-    y.ticks(3).forEach(d => {
-      context.moveTo(0, y(d));
-      context.lineTo(innerWidth, y(d));
-    });
-    context.strokeStyle = '#6d7182';
-    context.stroke();
+    const y = scaleLinear()
+      .domain([0, yMax * 1.1])
+      .range([innerHeight, 0]);
 
-    context.textAlign = 'left';
-    context.textBaseline = 'bottom';
-    y.ticks(3).forEach(d => {
-      context.fillStyle = '#ffffff';
-      context.fillText(pretty(d), 0, y(d) - 2);
-    });
+    // Add SVG and set dimensions
+    const svg = container.append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('preserveAspectRatio', 'xMidYMid')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .style('width', '100%');
+
+    // Add background element and apply definition
+    svg.append('rect')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('fill', 'url(#radial-gradient)');
+
+    const xAxis = axisBottom(x)
+      .tickValues(xTicks)
+      .tickFormat(d => germanDateShort(d));
+
+    const yAxis = axisRight(y)
+      .tickSize(width - margin.left - margin.right)
+      .tickFormat(d => pretty(d))
+      .ticks(3);
+
+    // Add axes
+    const axes = svg.append('g')
+      .classed('axes', true)
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    axes.append('g')
+      .call(yAxis)
+      .call(g => g.select('.domain').remove())
+      .call(g => g.selectAll('.tick line')
+        .attr('stroke', '#6d7182')
+      )
+      .call(g => g.selectAll('.tick text')
+        .attr('x', 0)
+        .attr('dy', -5)
+        .attr('font-family', '"Open Sans", sans-serif')
+        .attr('font-size', 14)
+        .attr('font-weight', 300)
+        .attr('fill', '#ffffff')
+      );
+
+    axes.append('g')
+      .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
+      .call(xAxis)
+      .call(g => g.select('.domain').remove())
+      .call(g => g.selectAll('.tick line').remove())
+      .call(g => g.selectAll('.tick text')
+        .attr('dy', 8)
+        .attr('font-family', '"Open Sans", sans-serif')
+        .attr('font-size', 14)
+        .attr('font-weight', 300)
+        .attr('fill', '#ffffff')
+      );
 
     // Draw bars
-    context.fillStyle = '#0b9fd8';
-    data.forEach(d => {
-      context.fillRect(x(d.date), y(d.value), x.bandwidth(), innerHeight - y(d.value));
-    });
+    svg.append('g')
+      .classed('bars', true)
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', d => x(d.date))
+      .attr('y', d => y(d.value))
+      .attr('width', x.bandwidth())
+      .attr('height', d => innerHeight - y(d.value))
+      // .attr('shape-rendering', 'crispEdges')
+      .attr('fill', '#0b9fd8');
 
     // Draw line
     const lineConstructor = line()
       .x(d => x(d.date) + (x.bandwidth() / 2))
       .y(d => y(d.value))
-      .curve(curveMonotoneX)
-      .context(context);
+      .curve(curveMonotoneX);
 
-    context.setLineDash([10, 10]);
-    context.beginPath();
-    context.lineCap = 'round';
-    lineConstructor(sma(data, 7, 'value'));
-    context.lineWidth = 3;
-    context.strokeStyle = '#ffffff';
-    context.stroke();
+    svg.append('g')
+      .classed('line', true)
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .append('path')
+      .attr('d', lineConstructor(sma(data, 7, 'value')))
+      .attr('fill', 'none')
+      .attr('stroke', '#ffffff')
+      .attr('stroke-width', 3)
+      .attr('stroke-dasharray', [10, 10])
+      .attr('stroke-linecap', 'round');
+    // Add title and description
+    const header = svg.append('g')
+      .classed('header', true)
+      .attr('transform', `translate(${margin.left}, 40)`);
 
-    // Add title
-    context.font = '600 24px "Open Sans", OpenSans, Arial';
-    context.textAlign = 'left';
-    context.textBaseline = 'top';
-    context.fillStyle = '#ffffff';
-    context.fillText(meta.title, 0, -margin.top + 20);
+    header.append('text')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('font-family', '"Open Sans", sans-serif')
+      .attr('font-size', 24)
+      .attr('font-weight', 600)
+      .attr('fill', '#ffffff')
+      .text(meta.title);
 
-    // Add description
-    context.font = '300 15px "Open Sans", OpenSans, Arial';
-    context.textAlign = 'left';
-    context.textBaseline = 'top';
-    context.fillStyle = '#9fa3b3';
-    context.fillText(meta.description, 0, -margin.top + 50);
+    header.append('text')
+      .attr('x', 0)
+      .attr('y', 25)
+      .attr('font-family', '"Open Sans", sans-serif')
+      .attr('font-size', 15)
+      .attr('font-weight', 300)
+      .attr('fill', '#9fa3b3')
+      .text(meta.description);
 
     // Add key
-    context.beginPath();
-    context.rect(0, -margin.top + 85, 11, 11);
-    context.fillStyle = '#0b9fd8';
-    context.fill();
+    const spacing = 155;
 
-    context.font = '300 15px "Open Sans", OpenSans, Arial';
-    context.textAlign = 'left';
-    context.textBaseline = 'top';
-    context.fillStyle = '#ffffff';
-    context.fillText('Neuinfektionen', 18, -margin.top + 85);
+    const key = svg.append('g')
+      .classed('key', true)
+      .attr('transform', `translate(${margin.left}, 90)`);
+    
+    key.append('rect')
+      .attr('x', 0 * spacing)
+      .attr('y', 2)
+      .attr('width', 12)
+      .attr('height', 12)
+      .attr('fill', '#0b9fd8');
 
-    context.setLineDash([5, 5]);
-    context.beginPath();
-    context.moveTo(145, -margin.top + 91);
-    context.lineTo(175, -margin.top + 91);
-    context.lineCap = 'round';
-    context.lineWidth = 3;
-    context.stroke();
+    key.append('text')
+      .attr('x', (0 * spacing) + 20)
+      .attr('dominant-baseline', 'hanging')
+      .attr('font-family', '"Open Sans", sans-serif')
+      .attr('font-size', 15)
+      .attr('font-weight', 300)
+      .attr('fill', '#ffffff')
+      .text('Neuinfektionen');
 
-    context.font = '300 15px "Open Sans", OpenSans, Arial';
-    context.textAlign = 'left';
-    context.textBaseline = 'top';
-    context.fillStyle = '#ffffff';
-    context.fillText('7-Tage-Mittelwert', 180, -margin.top + 85);
+    key.append('path')
+      .attr('d', `M ${1 * spacing} 8 L ${(1 * spacing) + 30} 8`)
+      .attr('fill', 'none')
+      .attr('stroke', '#ffffff')
+      .attr('stroke-width', 3)
+      .attr('stroke-dasharray', [5, 5])
+      .attr('stroke-linecap', 'round');
+
+    key.append('text')
+      .attr('x', (1 * spacing) + 35)
+      .attr('dominant-baseline', 'hanging')
+      .attr('font-family', '"Open Sans", sans-serif')
+      .attr('font-size', 15)
+      .attr('font-weight', 300)
+      .attr('fill', '#ffffff')
+      .text('7-Tage-Mittelwert');
 
     // Add author and source
-    context.font = '300 14px "Open Sans", OpenSans, Arial';
-    context.textAlign = 'left';
-    context.textBaseline = 'top';
-    context.fillStyle = '#9fa3b3';
-    context.fillText(`Grafik: ${meta.author}, Daten: ${meta.source} (Stand: ${germanDate(meta.date)})`, 0, innerHeight + 40);
+    const footer = svg.append('g')
+      .classed('footer', true)
+      .attr('transform', `translate(${margin.left}, ${height - 25})`);
 
-    // Scale canvas by pixel density
-    context.scale(1, 1);
-
-    // Create image from canvas and append it to the DOM
-    container.append('img')
-      .attr('src', canvas.toDataURL())
-      .attr('title', meta.title)
-      .attr('alt', `${meta.title}: ${meta.description}`);
+    footer.append('text')
+      .attr('font-family', '"Open Sans", sans-serif')
+      .attr('font-size', 14)
+      .attr('font-weight', 300)
+      .attr('fill', '#9fa3b3')
+      .text(`Grafik: ${meta.author}, Daten: ${meta.source} (Stand: ${germanDate(meta.date)})`);
   }
 
   update() {
