562ca67454a07dea14a21db50dc9a29bdc71f9bf
Steffen Kühne <stekhn@gmail.com>
2020-11-04 16:02:23 +0100

    Split text in three sections

--- src/scripts/text/bayern-text.js
+++ src/scripts/text/bayern-text.js
@@ -5,7 +5,7 @@ import { pretty, currentCount, currentIncrease, casesPer100Tsd7Days, trendClassi
 // '(+' + pretty(currentIncreasePerc(deathData)) + ' %)'
 
 export function init(config) {
-  const { caseTarget, deathTarget, caseData, recoveredData, deathData, metaData } = config;
+  const { caseTarget, reproTarget, deathTarget, caseData, recoveredData, deathData, metaData } = config;
   const reproData = reproNumber(sma(caseData));
   const confidenceInterval = confidence(reproData, '95');
   const reproValue = reproData[reproData.length - 2].value;
@@ -18,9 +18,9 @@ export function init(config) {
 
   Im Vergleich zur Vorwoche ist die Zahl der Neuinfektionen jedoch ${trendClassifier(weekTrend(caseData))} (<span class="${trendArrow(weekTrend(caseData))}"></span>${pretty((weekTrend(caseData) || 0), true)} %).
 
-  Damit kommt Bayern auf einen <a href="#inzidenz">Inzidenzwert</a> von ${pretty(casesPer100Tsd7Days(caseData, metaData.pop))} gemeldeten Fällen pro 100.000 Einwohner in den vergangenen sieben Tagen. In der Woche zuvor lag die 7-Tage-Inzidenz noch bei ${pretty(casesPer100Tsd7Days(caseData.slice(0, caseData.length-7), metaData.pop))} Fällen pro 100.000 Einwohner.<br><br>
+  Damit kommt Bayern auf einen <a href="#inzidenz">Inzidenzwert</a> von ${pretty(casesPer100Tsd7Days(caseData, metaData.pop))} gemeldeten Fällen pro 100.000 Einwohner in den vergangenen sieben Tagen. In der Woche zuvor lag die 7-Tage-Inzidenz noch bei ${pretty(casesPer100Tsd7Days(caseData.slice(0, caseData.length-7), metaData.pop))} Fällen pro 100.000 Einwohner.`;
 
-  Die <a href="#reproduktionszahl">Reproduktionszahl</a> für Bayern liegt nach Berechnungen von BR Data bei ungefähr ${pretty(reproValue, false, 'round', 100)}. Das bedeutet, dass jede infizierte Person durchschnittlich ${oneManyPersons(reproValue)} ansteckt. Diese Berechnung ist jedoch nur eine Schätzung, die bestimmten Abweichungen unterliegt. Deshalb ist es sinnvoll, das sogenannte Konfidenzintervall zu betrachten: Mit sehr hoher Wahrscheinlichkeit (95 %) liegt die Reproduktionszahl in einem Bereich von ${pretty(lowerReproValue, false, 'floor', 100)} bis ${pretty(upperReproValue, false, 'ceil', 100)}.<br><br>
+  const reproText = `Die <a href="#reproduktionszahl">Reproduktionszahl</a> für Bayern liegt nach Berechnungen von BR Data bei ungefähr ${pretty(reproValue, false, 'round', 100)}. Das bedeutet, dass jede infizierte Person durchschnittlich ${oneManyPersons(reproValue)} ansteckt. Diese Berechnung ist jedoch nur eine Schätzung, die bestimmten Abweichungen unterliegt. Deshalb ist es sinnvoll, das sogenannte Konfidenzintervall zu betrachten: Mit sehr hoher Wahrscheinlichkeit (95 %) liegt die Reproduktionszahl in einem Bereich von ${pretty(lowerReproValue, false, 'floor', 100)} bis ${pretty(upperReproValue, false, 'ceil', 100)}.<br><br>
 
   Nach Berechnungen des RKI sind mittlerweile wieder mindestens ${pretty(currentCount(recoveredData))} Menschen in Bayern <a href="#genesungen">genesen</a>.`;
 
@@ -34,6 +34,9 @@ export function init(config) {
   
   const caseElement = document.querySelector(caseTarget);
   caseElement.innerHTML = caseText;
+
+  const reproElement = document.querySelector(reproTarget);
+  reproElement.innerHTML = reproText;
 
   const deathElement = document.querySelector(deathTarget);
   deathElement.innerHTML = deathText;
