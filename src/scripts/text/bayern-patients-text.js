14db2dbc567d41f127cbe673f50489817ff27711
Steffen Kühne <stekhn@gmail.com>
2020-11-04 20:58:43 +0100

    Add more less classifier for ICU text

--- src/scripts/text/bayern-patients-text.js
+++ src/scripts/text/bayern-patients-text.js
@@ -7,7 +7,7 @@ export function init(config) {
   const currentIncrease = patientData[patientData.length - 1].faelleCovidAktuell - patientData[patientData.length - 2].faelleCovidAktuell;
   const currentWeekTrend = weekTrend(patientData, 10, 'faelleCovidAktuell');
   
-  const text = `Ein weiterer wichtiger Indikator zur Einschätzung der Pandemie ist die Zahl der gemeldeten Corona-Patienten in intensivmedizinischer Behandlung. Nach Angaben des DIVI-Intensivregisters werden derzeit ${pretty(currentPatients)} Menschen in Bayern intensivmedizinsch wegen einer Corona-Erkrankung behandelt. Das sind ${pretty(currentIncrease)} Intensivpatienten mehr als noch am Vortag.
+  const text = `Ein weiterer wichtiger Indikator zur Einschätzung der Pandemie ist die Zahl der <a href="#intensivpatienten">Corona-Patienten</a>, die wegen der Schwere ihrer Erkrankung auf einer Intensivstation behandelt werden müssen. Nach Angaben des DIVI-Intensivregisters befinden sich derzeit ${pretty(currentPatients)} Menschen wegen Corona in intensivmedizinischer Behandlung. Das sind ${pretty(currentIncrease)} Intensivpatienten ${moreLess(currentIncrease)} als noch am Vortag.
   
   Im Vergleich zur Vorwoche ist die Zahl der Intensivpatienten ${trendClassifier(currentWeekTrend)} (<span class="${trendArrow(currentWeekTrend)}"></span>${pretty((currentWeekTrend || 0), true)} %).
   
@@ -15,4 +15,14 @@ export function init(config) {
   
   const textElement = document.querySelector(target);
   textElement.innerHTML = text;
+}
+
+function moreLess(value) {
+  if (value < 0) {
+    return 'weniger';
+  } else if (value === 0) {
+    return 'mehr';
+  } else if (value > 0) {
+    return 'mehr';
+  }
 }
--- src/scripts/text/deutschland-patients-text.js
+++ src/scripts/text/deutschland-patients-text.js
@@ -7,12 +7,22 @@ export function init(config) {
   const currentIncrease = patientData[patientData.length - 1].faelleCovidAktuell - patientData[patientData.length - 2].faelleCovidAktuell;
   const currentWeekTrend = weekTrend(patientData, 10, 'faelleCovidAktuell');
   
-  const text = `Ein weiterer wichtiger Indikator zur Einschätzung der Pandemie ist die Zahl der gemeldeten Corona-Patienten in intensivmedizinischer Behandlung. Nach Angaben des DIVI-Intensivregisters werden bundesweit ${pretty(currentPatients)} Menschen intensivmedizinsch wegen einer Corona-Erkrankung behandelt. Das sind ${pretty(currentIncrease)} Intensivpatienten mehr als noch am Vortag.
+  const text = `Ein weiterer wichtiger Indikator zur Einschätzung der Pandemie ist die Zahl der <a href="#intensivpatienten">Corona-Patienten</a>, die wegen der Schwere ihrer Erkrankung auf einer Intensivstation behandelt werden müssen. Nach Angaben des DIVI-Intensivregisters befinden sich bundesweit ${pretty(currentPatients)} Menschen wegen Corona in intensivmedizinischer Behandlung. Das sind ${pretty(Math.abs(currentIncrease))} Intensivpatienten ${moreLess(currentIncrease)} als noch am Vortag.
   
   Im Vergleich zur Vorwoche ist die Zahl der Intensivpatienten ${trendClassifier(currentWeekTrend)} (<span class="${trendArrow(currentWeekTrend)}"></span>${pretty((currentWeekTrend || 0), true)} %).
   
-  Deutschlandweit gibt es momentan ungefähr ${currentPatientsPer100k} Intensivpatienten pro 100.000 Einwohner.`;
+  Deutschlandweit gibt es momentan ungefähr ${pretty(currentPatientsPer100k)} Intensivpatienten pro 100.000 Einwohner.`;
   
   const textElement = document.querySelector(target);
   textElement.innerHTML = text;
+}
+
+function moreLess(value) {
+  if (value < 0) {
+    return 'weniger';
+  } else if (value === 0) {
+    return 'mehr';
+  } else if (value > 0) {
+    return 'mehr';
+  }
 }
