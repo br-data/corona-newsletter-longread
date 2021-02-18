083be449ecd3217ce0189495f2b520c66b3cbc85
Michael Kreil <github@michael-kreil.de>
2021-02-18 17:03:43 +0100

    don't need that await

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -197,11 +197,11 @@ async function init() {
 
   // Table for Bavarian counties (Landkreise)
   (async function () {
-    const bayernLkrCasesRequest = await fetch(`${apiUrl}?startDate=${startDateString}&endDate=${endDateString}&newCases=true&group=Landkreis&bundesland=Bayern`)
+    const bayernLkrCasesRequest = fetch(`${apiUrl}?startDate=${startDateString}&endDate=${endDateString}&newCases=true&group=Landkreis&bundesland=Bayern`)
       .then(response => response.json())
       .catch(logError);
 
-    const bayernLkrDeathsRequest = await fetch(`${apiUrl}?startDate=${startDateString}&endDate=${endDateString}&newCases=true&group=Landkreis&bundesland=Bayern&sumField=AnzahlTodesfall`)
+    const bayernLkrDeathsRequest = fetch(`${apiUrl}?startDate=${startDateString}&endDate=${endDateString}&newCases=true&group=Landkreis&bundesland=Bayern&sumField=AnzahlTodesfall`)
       .then(response => response.json())
       .catch(logError);
 
