c1ea5a942fe0b9d257931c762b1987164d515655
Steffen Kühne <stekhn@gmail.com>
2021-02-23 15:15:51 +0100

    Update URL for data service API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -43,7 +43,7 @@ window.addEventListener('load', init);
 async function init() {
   const logError = error => console.warn(error);
   
-  const apiUrl = 'https://rki-api-v2.live.brdata-live.de/query';
+  const apiUrl = 'https://corona-deutschland-api.interaktiv.br.de/';
 
   const urlParams = new URLSearchParams(window.location.search);
   const startDate = new Date(urlParams.get('startDate') || new Date('2020-02-25'));
