53686630d2c16b80b6a8cc624893eb2ede62661f
Michael Kreil <github@michael-kreil.de>
2021-04-30 18:15:58 +0200

    rename variables

--- src/scripts/text/deutschland-vaccinations-text.js
+++ src/scripts/text/deutschland-vaccinations-text.js
@@ -3,17 +3,17 @@ import { pretty, currentCount, trendClassifier } from '../utils';
 export function init(config) {
   const { target, data } = config;
 
-  const firstShotCount = currentCount(data, 'personen_erst_kumulativ');
-  const secondShotCount = currentCount(data, 'personen_voll_kumulativ');
-  const firstShotPerc = currentCount(data, 'impf_quote_erst');
-  const secondShotPerc = currentCount(data, 'impf_quote_voll');
+  const partVaccCount = currentCount(data, 'personen_erst_kumulativ');
+  const fullVaccCount = currentCount(data, 'personen_voll_kumulativ');
+  const partVaccPerc = currentCount(data, 'impf_quote_erst');
+  const fullVaccPerc = currentCount(data, 'impf_quote_voll');
   const totalShots = currentCount(data, 'dosen_kumulativ');
   const currentIncrease = data[data.length - 1].dosen_kumulativ - data[data.length - 2].dosen_kumulativ;
   const currentWeekSum = data[data.length - 1].dosen_kumulativ - data[data.length - 6].dosen_kumulativ;
   const previousWeekSum = data[data.length - 7].dosen_kumulativ - data[data.length - 12].dosen_kumulativ;
   const currentWeekTrend = ((currentWeekSum - previousWeekSum) / previousWeekSum) * 100;
   
-  const text = `Seit Beginn der Impfkampagne haben ${pretty(firstShotCount)} Menschen (insgesamt ${pretty(firstShotPerc)} %) in Deutschland mindestens eine erste <a href="#impfungen">Impfdosis</a> erhalten. Davon sind ${pretty(secondShotCount)} Personen (insgesamt ${pretty(secondShotPerc)} %) inzwischen vollständig geimpft.
+  const text = `Seit Beginn der Impfkampagne haben ${pretty(partVaccCount)} Menschen (insgesamt ${pretty(partVaccPerc)} %) in Deutschland mindestens eine erste <a href="#impfungen">Impfdosis</a> erhalten. Davon sind ${pretty(fullVaccCount)} Personen (insgesamt ${pretty(fullVaccPerc)} %) inzwischen vollständig geimpft.
   
   Insgesamt wurden bisher ${pretty(totalShots)} Impfdosen verabreicht. Das sind ${pretty(currentIncrease)} Impfdosen mehr als noch am Vortag.
   
