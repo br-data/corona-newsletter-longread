89773aa857dbf82f736c26f1582ed15d7333b371
Steffen Kühne <stekhn@gmail.com>
2022-01-26 20:49:02 +0100

    Update vaccinations texts

--- src/scripts/text/bayern-vaccinations-text.js
+++ src/scripts/text/bayern-vaccinations-text.js
@@ -3,17 +3,19 @@ import { pretty, currentCount, trendClassifier } from '../utils';
 export function init(config) {
   const { target, data, metaData } = config;
 
-  const minOneShotCount = currentCount(data, 'personen_min1_kumulativ');
-  const fullVaccCount = currentCount(data, 'personen_voll_kumulativ');
-  const minOneShotPerc = minOneShotCount*100/metaData.pop;
-  const fullVaccPerc = fullVaccCount*100/metaData.pop;
+  const firstShotCount = currentCount(data, 'personen_min1_kumulativ');
+  const secondShotCount = currentCount(data, 'personen_voll_kumulativ');
+  const thirdShotCount = currentCount(data, 'personen_auffr_kumulativ');
   const totalShots = currentCount(data, 'dosen_kumulativ');
+  const firstShotPerc = firstShotCount * 100 / metaData.pop;
+  const secondShotPerc = secondShotCount * 100 / metaData.pop;
+  const thirdShotPerc = thirdShotCount * 100 / metaData.pop;
   const currentIncrease = data[data.length - 1].dosen_kumulativ - data[data.length - 2].dosen_kumulativ;
   const currentWeekSum = data[data.length - 1].dosen_kumulativ - data[data.length - 6].dosen_kumulativ;
   const previousWeekSum = data[data.length - 7].dosen_kumulativ - data[data.length - 12].dosen_kumulativ;
   const currentWeekTrend = ((currentWeekSum - previousWeekSum) / previousWeekSum) * 100;
   
-  const text = `Seit Beginn der Impfkampagne haben ${pretty(minOneShotCount)} Menschen (insgesamt ${pretty(minOneShotPerc)} %) in Bayern mindestens eine <a href="#impfungen">Impfdosis</a> erhalten. Davon sind ${pretty(fullVaccCount)} Personen (insgesamt ${pretty(fullVaccPerc)} %) inzwischen vollständig geimpft.
+  const text = `Seit Beginn der Impfkampagne haben mindestens ${pretty(firstShotCount)} Menschen in Bayern eine <a href="#impfungen">Erstimpfung</a> erhalten. Das entspricht ${pretty(firstShotPerc)} % der bayerischen Bevölkerung. ${pretty(secondShotCount)} Menschen (${pretty(secondShotPerc)} %) haben eine Zweitimpfung erhalten und gelten laut RKI als grundimmunisiert. ${pretty(thirdShotCount)} Personen (${pretty(thirdShotPerc)} %) haben eine Drittimpfung, die sogenannten Auffrischimpfung oder Booster, bekommen.
   
   Insgesamt wurden bisher ${pretty(totalShots)} Impfdosen verabreicht. Das sind ${pretty(currentIncrease)} Impfdosen mehr als noch am Vortag.
   
